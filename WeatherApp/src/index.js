ecc811060d4e9373eb5f21347584a89ccc8a57b1
Cole <coleb@outlook.com>
2021-01-07 15:40:39 -0700

    added functionality to interact with the api and display the data on the webpage

--- WeatherApp/src/index.js
+++ WeatherApp/src/index.js
@@ -1,9 +1,5 @@
+const { temperature } = require("./modules/api");
 
+import setDocument from './modules/api';
 
-async function getWeather() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?id=5913490&appid=812094d10dc57aeb1bbaf6899556b3c6', {mode: 'cors'});
-    const result = await response.json();
-    return result;
-}
-
-console.log(getWeather());
+setDocument();
---
+++ WeatherApp/src/modules/api.js
@@ -0,0 +1,23 @@
+async function getWeatherJson() {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?id=5913490&appid=812094d10dc57aeb1bbaf6899556b3c6', {mode: 'cors'});
+    const result = await response.json();
+    console.log(result);
+    return result;
+}
+
+function setElement(id, temp) {
+    document.getElementById(id).innerText = temp;
+}
+
+
+function setDocument() {
+    let weather = getWeatherJson();
+    weather.then(function(data) {
+        setElement("temperature", data['main']['temp']);
+        setElement("feels-like", data['main']['feels_like']);
+        setElement("weather", data['weather'][0]['description']);
+        setElement("wind", data['wind']['speed']);
+    });
+    
+}
+export default setDocument;
